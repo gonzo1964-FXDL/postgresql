0b3c3fc06e27245b0997a04e4d0c4f54ec16ed7c
Michael Spruck <michael.spruck@googlemail.com>
2023-01-02 14:38:03 +0100

    Erweiterung mit funktionen

--- select_journalV1.py
+++ select_journalV1.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
-
+#
+#       erweiteruzngen:
+#       add update function
+#       update Journal set erfolg = 'Python und Postgres' where datum = '27.12.2022' ;
+#
+#       für den täglichen update eine Datei zur Eingabe erzeugen
+#
 import psycopg2
 from sys import argv
 
@@ -8,29 +14,46 @@ from sys import argv
 def selDatum(daTum):
     print ("--Eintraege fuer Datum =", daTum) 
 #
-# erweiterung zur abfrage eines bestimmten datums
+def outPut(row):
+    print ("--------------------------------------")
+    print ("Datum = ", row[0])
+    print ("Erfolg = ", row[1])
+    print ("Sport = ", row[2])
+    print ("Buch = ", row[3])
+    print ("Erkenntnis = ", row [4], "\n")
+    print ("---------------------------------------")
+    #
+#
+def inputfile(filename):
+    print ("Dateiname = ")
+    fobj = open("journal.txt")
+    for line in fobj:
+        Basket = line.split(":")
+        print(Basket[0])
+        print(Basket[1])
+        print(Basket[2])
+# Erweiterung zur abfrage eines bestimmten datums
+
 #print ("---> Abfrage Datum =", argv[1])
 if len(argv) > 1:
     print ("---> Abfrage Datum =", argv[1])  
     daTum = argv[1]
     selDatum(daTum)
+    filename = "journal.txt"
+    inputfile(filename)
+#    
+#   abfrage ob argv2 einen Wert hat zu, einlesen der Datei
+#
     try:
         connection = psycopg2.connect(database = "MSTemp01", user = "postgres", password = "postgres", host = "localhost", port = "5432")
         print ("Opened database successfully")
-
         cursor = connection.cursor()
         #print (daTum)
-        
         cursor.execute("""SELECT * from journal where datum='%s'""" % (daTum)) 
         #cursor.execute("SELECT * from journal where datum ='01.01.2023'") 
         rows = cursor.fetchall()
         for row in rows:
-            #print ("Datum = ", row[0])
-            #print ("Erfolg = ", row[1])
-            #print ("Sport = ", row[2])
-            #print ("Buch = ", row[3])
-            #print ("Erkenntnis = ", row [4], "\n")
-            #
+            outPut(row)
             print (row[0] , ":" , row[1], ":" , row[2] , ":" , row[3] , ":", row[4])
         #print ("Operation done successfully")
     except (Exception, psycopg2.Error) as error:
@@ -49,15 +72,10 @@ else:
         print ("Opened database successfully")
         cursor = connection.cursor()
 
-        cursor.execute("SELECT datum, erfolg, sport, buch, erkenntnis  from journal")
+        cursor.execute("SELECT datum, erfolg, sport, buch, erkenntnis from journal")
         rows = cursor.fetchall()
+        outPut(rows)
         for row in rows:
-            #print ("Datum = ", row[0])
-            #print ("Erfolg = ", row[1])
-            #print ("Sport = ", row[2])
-            #print ("Buch = ", row[3])
-            #print ("Erkenntnis = ", row [4], "\n")
-            #
             print (row[0] , ":" , row[1], ":" , row[2] , ":" , row[3] , ":", row[4])
         #print ("Operation done successfully")
     except (Exception, psycopg2.Error) as error:

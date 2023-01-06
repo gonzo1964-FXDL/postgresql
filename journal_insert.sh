7f56e2b99590a3f549a63154458e94b27f6369f3
Michael Spruck <michael.spruck@googlemail.com>
2023-01-06 10:11:32 +0100

    insert für jorunal db

--- journal_insert.sh
+++ journal_insert.sh
@@ -1,2 +1,2 @@
- python3 journal_eingabe_neu.py 03.01.2023 'Python, VSCode auf Macbook Air' '' '' ''
-  
+python3 journal_eingabe_neu.py 04.01.2023 'Termin Standesamt' '' '' ''
+python3 journal_eingabe_neu.py 05.01.2023 'Python csv file lesen und DB schreiben, Parallels installiert auf Mac mit Windowsr' '' '' ''

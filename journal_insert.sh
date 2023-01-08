abcd3427c7a2747d24bf75f188233f3e272ace64
Michael Spruck <michael.spruck@googlemail.com>
2023-01-08 12:22:42 +0100

    journal insert.sh update

--- journal_insert.sh
+++ journal_insert.sh
@@ -1,2 +1,2 @@
-python3 journal_eingabe_neu.py 04.01.2023 'Termin Standesamt' '' '' ''
-python3 journal_eingabe_neu.py 05.01.2023 'Python csv file lesen und DB schreiben, Parallels installiert auf Mac mit Windowsr' '' '' ''
+python3 journal_eingabe_neu.py 06.01.2023 'MacBook Parallels Windows Control-M installieren' '' '' ''
+python3 journal_eingabe_neu.py 07.01.2023 'Kawasaki Interessent da, Mac/Win VCStudio mit Git, Elena tefoniert, Mühldorf Kaffee, TopGun' '' '' ''

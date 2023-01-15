3b111a09157474f64ed648fe29779717f85db7c2
Michael Spruck <michael.spruck@googlemail.com>
2023-01-15 09:34:51 +0100

    Journal script

--- journal_insert.sh
+++ journal_insert.sh
@@ -1,2 +1,4 @@
-python3 journal_eingabe_neu.py 09.01.2023 'Versuch Control-M auf DELL/Mint zu insnstallieren, Elena Hausbesuch, Ubuntu installiert' 'Stütz, Katzenbuckel, seitliche Übungen, situps' '' ''
-python3 journal_eingabe_neu.py 10.01.2023 'Liux Mint, Roseline Sex' '' '' ''
+python3 journal_eingabe_neu.py 11.01.2023 'Had-Eck Dyno gebracht' '' '' ''
+python3 journal_eingabe_neu.py 12.01.2023 '' 'Stütz, Katzenbuckel, seitliche Übungen, situps' '' ''
+python3 journal_eingabe_neu.py 13.01.2023 'Burger King Dorfen' '' '' ''
+python3 journal_eingabe_neu.py 14.01.2023 'Roselione Sex, Freising gefahren' '' '' ''

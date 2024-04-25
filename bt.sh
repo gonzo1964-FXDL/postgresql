2db2ca391bf40afb45bcf1c666bf5354bce71dc4
Michael Spruck <michael.spruck@googlemail.com>
2024-04-25 08:03:16 +0200

    update

--- bt.sh
+++ bt.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-gatekeeper_status=$(spctl --status)
-
-if [[ ${gatekeeper_status} ==  "assessments enabled" ]]
-then
-    echo "Gatekeeper is enabled do some stuff"
-else
-    echo "Gatekeeper is disabled do some stuff"
-fi
-
-
-echo $1
-
 if [[ ${1} ==  "lesen" ]]
 then
     echo "lesen"
